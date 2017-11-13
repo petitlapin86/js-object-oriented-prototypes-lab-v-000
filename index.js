ec61db30d421155d4e1b2ecaae14c572004d5512
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-13 21:52:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@
      return "You will see great things from Scuber."
   }
 
-   BoardMember.prototype..sayHi = function(){
+   BoardMember.prototype.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
